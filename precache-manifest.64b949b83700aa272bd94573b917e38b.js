eecc226971ca8daae04b65069edc45affe1fdb5e
harshil35 <harshilap2004@gmail.com>
2022-07-16 18:23:35 +0300

    Updates

---
+++ precache-manifest.64b949b83700aa272bd94573b917e38b.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = [
+  {
+    "revision": "242aeaed38787064eff0",
+    "url": "/Search-agent/static/css/main.74a95e79.chunk.css"
+  },
+  {
+    "revision": "242aeaed38787064eff0",
+    "url": "/Search-agent/static/js/main.242aeaed.chunk.js"
+  },
+  {
+    "revision": "0b5adef5a8e99730695f",
+    "url": "/Search-agent/static/js/1.0b5adef5.chunk.js"
+  },
+  {
+    "revision": "4288b75f908d493a1e3e",
+    "url": "/Search-agent/static/css/2.8b932f89.chunk.css"
+  },
+  {
+    "revision": "4288b75f908d493a1e3e",
+    "url": "/Search-agent/static/js/2.4288b75f.chunk.js"
+  },
+  {
+    "revision": "1294c0203c50831b60cb",
+    "url": "/Search-agent/static/js/runtime~main.1294c020.js"
+  },
+  {
+    "revision": "a4db3ef0dcdefcdd76e5f0902c8fd637",
+    "url": "/Search-agent/static/media/among1.a4db3ef0.png"
+  },
+  {
+    "revision": "d51779f024db2a8273af97a25e96d623",
+    "url": "/Search-agent/static/media/among10.d51779f0.png"
+  },
+  {
+    "revision": "2d7605c90868f61713538af027b8c771",
+    "url": "/Search-agent/static/media/among2.2d7605c9.png"
+  },
+  {
+    "revision": "c768d7e729cf6d17ca6be256196b9b56",
+    "url": "/Search-agent/static/media/among3.c768d7e7.png"
+  },
+  {
+    "revision": "15fbf345ffa27b8b04bafe122bf5cead",
+    "url": "/Search-agent/static/media/among4.15fbf345.png"
+  },
+  {
+    "revision": "1f19111ab93fe854d7e90732e730b206",
+    "url": "/Search-agent/static/media/among5.1f19111a.png"
+  },
+  {
+    "revision": "ca801a860d0bc507e21b2f4a1b0d463f",
+    "url": "/Search-agent/static/media/among6.ca801a86.png"
+  },
+  {
+    "revision": "5e781a513f9aec2221745730dbc5d547",
+    "url": "/Search-agent/static/media/among7.5e781a51.png"
+  },
+  {
+    "revision": "97003e491351f95e1e893bb99f90a677",
+    "url": "/Search-agent/static/media/among8.97003e49.png"
+  },
+  {
+    "revision": "7a1493ac8cd8883a721b7a302ac9a207",
+    "url": "/Search-agent/static/media/among9.7a1493ac.png"
+  },
+  {
+    "revision": "379fc55113e3e079a6b0181d2bb691a8",
+    "url": "/Search-agent/static/media/GAELA.379fc551.otf"
+  },
+  {
+    "revision": "f059c285e6fc122db4fbbdaff49d8e82",
+    "url": "/Search-agent/index.html"
+  }
+];
